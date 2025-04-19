d2dbe9ba17f9b90a394d29575a04e27545a74888
erdilhasan <hasanerdil1260@gmail.com>
2025-04-20 00:25:09 +0300

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./pages/Layout.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
