7d1481213c904e8b4b29d291b8592988523747b1
erdilhasan <hasanerdil1260@gmail.com>
2025-04-20 16:10:12 +0300

    Total cost calculation implemented

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,13 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useOutletContext } from "react-router";
 import CartItem from "../components/CartItem";
 
 export default function CartPage(params) {
   const { cart, setCart } = useOutletContext();
+  const [totalCost, setTotalCost] = useState(0);
 
   useEffect(() => {
     console.log(cart);
+    setTotalCost(getTotalCost());
   }, []);
+
+  const getTotalCost = () => {
+    return cart.reduce(
+      (sum, product) => sum + product.price * product.quantity,
+      0
+    );
+  };
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -18,6 +27,7 @@ export default function CartPage(params) {
       ) : (
         <h1 className="">Your Cart Is Empty</h1>
       )}
+
       {cart.length != 0 ? (
         <Link to={"/checkout"}>
           <button className="mt-2 bg-blue-500 text-white py-1 px-4 rounded">
@@ -29,6 +39,7 @@ export default function CartPage(params) {
           Add items to proceed to checkout
         </h2>
       )}
+      <h2>Total:{totalCost}</h2>
     </div>
   );
 }
