23ef526ace95758b8442b719b1fc59838cbff554
erdilhasan <hasanerdil1260@gmail.com>
2025-04-20 16:50:07 +0300

    Layout File refactored for cleaner code

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,6 +3,7 @@ import { FaHome, FaProductHunt, FaStar } from "react-icons/fa";
 import { FaBasketShopping } from "react-icons/fa6";
 import { MdAccountCircle } from "react-icons/md";
 import { Link, Outlet } from "react-router";
+import NavBar from "../components/NavBar";
 
 export default function Layout() {
   //global cart
@@ -18,31 +19,7 @@ export default function Layout() {
 
   return (
     <div className="">
-      <nav className="flex shadow-md rounded-md bg-white  m-0 content-between justify-between p-2">
-        <ul className="flex   justify-between content-between  ">
-          <li className="m-auto px-0">
-            <Link className="flex" to="/">
-              <FaHome className="m-auto"></FaHome> Home
-            </Link>
-          </li>
-
-          <li className="px-4">
-            <Link className="flex" to="/allproducts">
-              <FaProductHunt className="m-auto"></FaProductHunt> All Products
-            </Link>
-          </li>
-          <li className="px-4">
-            <Link className="flex" to="/cart">
-              <FaBasketShopping className="m-auto"></FaBasketShopping> Cart
-            </Link>
-          </li>
-          <li className="px-4">
-            <Link className="flex" to="/favourites">
-              <FaStar className="m-auto"></FaStar> Favourites
-            </Link>
-          </li>
-        </ul>{" "}
-      </nav>
+      <NavBar />
       <Outlet context={{ cart, setCart, favourites, setFavourites }}></Outlet>
     </div>
   );
