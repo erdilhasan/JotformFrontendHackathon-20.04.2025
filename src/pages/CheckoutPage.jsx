a9d062c8a22fd0ed91285d70eb38e6028849aa3d
erdilhasan <hasanerdil1260@gmail.com>
2025-04-20 16:47:11 +0300

    Fixed submission Submission now submits to the form with full name and address

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -12,10 +12,35 @@ export default function CheckoutPage(params) {
     formState: { errors },
   } = useForm();
 
+  const apiKey = import.meta.env.VITE_JOTFORM_API_KEY;
+  const form_id = import.meta.env.VITE_FORM_ID;
+
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     console.log({ ...data, cart: cart });
-    setIsSubmitted(true);
+
+    const formData = new URLSearchParams();
+    formData.append("submission[34]", data.fullname);
+    formData.append("submission[36]", data.address);
+
+    try {
+      const response = await fetch(
+        `https://api.jotform.com/form/${form_id}/submissions?apiKey=${apiKey}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: formData.toString(),
+        }
+      );
+
+      const data = await response.json();
+      console.log("Submission successful:", data);
+      setIsSubmitted(true);
+    } catch (error) {
+      console.error("Submission failed:", error);
+    }
   };
   return (
     <div>
@@ -32,20 +57,27 @@ export default function CheckoutPage(params) {
           <label>
             {" "}
             Full Name:{" "}
-            <input className="border-2 rounded-md" {...register("fullname")} />
+            <input
+              className="border-2 rounded-md"
+              {...register("fullname", { required: true })}
+            />
           </label>
 
           {/* include validation with required or other standard HTML validation rules */}
 
           <label>
             {" "}
-            Address: <input {...register("address", { required: true })} />
+            Address:{" "}
+            <input
+              className="border-2 rounded-md"
+              {...register("address", { required: true })}
+            />
           </label>
 
           {/* errors will return when field validation fails  */}
           {errors.exampleRequired && <span>This field is required</span>}
 
-          <input type="submit" />
+          <input className="border-2 rounded-2xl p-2" type="submit" />
         </form>
       )}
     </div>
